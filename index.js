e1450b3b1183d3f847132e3f60877a117b8e2fed
r21dm <rockydwimakmur@gmail.com>
2022-02-20 19:52:14 +0700

    Initialize Register function

--- index.js
+++ index.js
@@ -8,18 +8,32 @@ const connection = require("./database");
 
 const PORT = 8000;
 
+//Initialize & Setting Express
 app.use(express.json());
 app.use(cors());
 
-// test db connection
+//Test db connection
 database.connect((error) => {
   if (error) console.log(error);
 
   console.log("Connected at thread id", database.threadId);
 });
 
-//Controller POST
-app.post("/", (req, res) => {
+//GET username and email data
+app.get("/register", (req, res) => {
+  const USERNAME = req.query.username;
+  const EMAIL = req.query.email;
+  const QUERY = `SELECT * FROM db_pharmacy.user WHERE username="${USERNAME}" OR email="${EMAIL}"`;
+
+  console.log(QUERY);
+
+  connection.query(QUERY, (err, result) => {
+    res.status(200).send(result);
+  });
+});
+
+//POST Register
+app.post("/register", (req, res) => {
   console.log(req.body);
   const USERNAME = req.body.username;
   const PASSWORD = req.body.password;
@@ -44,6 +58,7 @@ app.post("/", (req, res) => {
   });
 });
 
+//Start Server
 server.listen(PORT, () => {
   console.log("Socket server is running at port:", PORT);
 });
