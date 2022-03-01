913c7b2f3ad001b1063d74119c643138e26d55bb
r21dm <rockydwimakmur@gmail.com>
2022-03-02 02:33:49 +0700

    Bug fix controller var db

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,4 @@
-const { db } = require("../database");
+const db = require("../database");
 require("dotenv").config();
 
 module.exports = {
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const { db } = require("../database");
+const db = require("../database");
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 
