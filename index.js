52cb55b124df787f98dd06ce294e83486c4cc2da
r21dm <rockydwimakmur@gmail.com>
2022-03-11 02:10:48 +0700

    Merge with other features

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const cors = require("cors");
-// const http = require("http");
-// const server = http.createServer(app);
-// const db = require("./database");
-//PORT
-const PORT = process.env.PORT || 2000;
-// const PORT = 8000;
-
+const http = require("http");
 const app = express();
+const server = http.createServer(app);
+const db = require("./database");
+require("dotenv").config();
+//PORT
+const PORT = process.env.PORT | 0;
+// const PORT = 8000;
 
 // Function to serve all static files
 // inside public directory.
@@ -28,21 +28,21 @@ const {
   cartRouters,
 } = require("./routers");
 
-// //Test db connection
-// db.connect((error) => {
-//   if (error) console.log(error);
+//Test db connection
+db.connect((error) => {
+  if (error) console.log(error);
 
-//   console.log("Connected at thread id", db.threadId);
-// });
+  console.log("Connected at thread id", db.threadId);
+});
 
 app.use("/users", userRouters);
 app.use("/products", productRouters);
 app.use("/", registerRouter);
 app.use("/cart", cartRouters);
 
-app.listen(PORT, () => console.log("Api Running :", PORT));
+// app.listen(PORT, () => console.log("Api Running :", PORT));
 
-// //Start Server
-// server.listen(PORT, () => {
-//   console.log("Socket server is running at port:", PORT);
-// });
+//Start Server
+server.listen(PORT, () => {
+  console.log("Socket server is running at port:", PORT);
+});
