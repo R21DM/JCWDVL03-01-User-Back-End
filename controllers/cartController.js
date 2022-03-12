e0a0920d805dffdc9dfd68ac62426098aca72b36
r21dm <rockydwimakmur@gmail.com>
2022-03-13 00:23:42 +0700

    Add delete & update cart

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 //GET user cart
 const userCart = (req, res) => {
   const ID = req.query.id;
-  const QUERY = `SELECT * FROM db_pharmacy.cart INNER JOIN db_pharmacy.product ON cart.product_id = product.id WHERE user_id=${ID};`;
+  const QUERY = `SELECT cart.*, product.name, product.price, cart.qty*product.price as total_price FROM db_pharmacy.cart INNER JOIN db_pharmacy.product ON cart.product_id = product.id WHERE user_id=${ID};`;
 
   db.query(QUERY, (err, result) => {
     let totalPrice = 0;
@@ -30,8 +30,7 @@ const addToCart = (req, res) => {
   const USERID = req.body.userId;
   const PRODUCTID = req.body.productId;
   const QTY = req.body.qty;
-  const PRICE = req.body.price;
-  const QUERY = `INSERT INTO db_pharmacy.cart (\`user_id\`, \`product_id\`, \`qty\`, \`price\`) VALUES (${USERID}, ${PRODUCTID}, ${QTY}, ${PRICE});`;
+  const QUERY = `INSERT INTO db_pharmacy.cart (\`user_id\`, \`product_id\`, \`qty\`) VALUES (${USERID}, ${PRODUCTID}, ${QTY});`;
 
   console.log(QUERY);
 
@@ -40,4 +39,29 @@ const addToCart = (req, res) => {
   });
 };
 
-module.exports = { addToCart, userCart };
+//DELETE cart data
+const deleteCart = (req, res) => {
+  const ID = req.query.id;
+  const QUERY = `DELETE FROM db_pharmacy.cart WHERE id = ${ID};`;
+
+  console.log(QUERY);
+
+  db.query(QUERY, (err, result) => {
+    res.status(200).send(result);
+  });
+};
+
+//UPDATE cart data
+const updateCart = (req, res) => {
+  const QTY = req.body.qty;
+  const ID = req.body.id;
+  const QUERY = `UPDATE db_pharmacy.cart SET qty = ${QTY} WHERE id = ${ID};`;
+
+  console.log(QUERY);
+
+  db.query(QUERY, (err, result) => {
+    res.status(200).send(result);
+  });
+};
+
+module.exports = { addToCart, userCart, deleteCart, updateCart };
--- routers/cartRouters.js
+++ routers/cartRouters.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const { router } = require("json-server");
 const { cartController } = require("../controllers");
 const routers = express.Router();
 
 routers.get("/", cartController.userCart);
 routers.post("/", cartController.addToCart);
+routers.delete("/", cartController.deleteCart);
+routers.patch("/", cartController.updateCart);
 
 module.exports = routers;
