5a2b9b7eb88dfa1bfcb81e1c8e50d88e58a2d734
alfathsien123 <alfaths77@gmail.com>
2022-03-14 22:11:41 +0700

    Before push to Master V1.0

--- database/index.js
+++ database/index.js
@@ -1,28 +1,19 @@
 const mysql = require("mysql");
 
-// const db = mysql.createPool({
-//   host: "127.0.0.1",
-//   user: "root",
-//   password: "@Alfath123",
-//   database: "db_pharmacy",
-//   port: 3306,
-//   multipleStatements: true,
-// });
-
-// db.getConnection((err, connection) => {
-//   if (err) {
-//     return console.error(`error : ${err.message}`);
-//   }
-//   console.log(`Connected to MySQL Server`);
-// });
-
-// module.exports = db;
-
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "test",
-  password: "test",
+const db = mysql.createPool({
+  host: "127.0.0.1" || "localhost",
+  user: "root" || "test",
+  password: "@Alfath123" || "test",
   database: "db_pharmacy",
+  port: 3306,
+  multipleStatements: true,
 });
 
-module.exports = connection;
+db.getConnection((err, connection) => {
+  if (err) {
+    return console.error(`error : ${err.message}`);
+  }
+  console.log(`Connected to MySQL Server`);
+});
+
+module.exports = db;
--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const multer = require("multer");
 const cors = require("cors");
-const http = require("http");
 const app = express();
-const server = http.createServer(app);
 const db = require("./database");
 require("dotenv").config();
-//PORT
+
 const PORT = process.env.PORT | 0;
-// const PORT = 8000;
 
 // Function to serve all static files
 // inside public directory.
@@ -59,13 +56,6 @@ const {
   historyRouters,
 } = require("./routers");
 
-//Test db connection
-db.connect((error) => {
-  if (error) console.log(error);
-
-  console.log("Connected at thread id", db.threadId);
-});
-
 app.use("/users", userRouters);
 app.use("/products", productRouters);
 app.use("/", registerRouter);
@@ -73,9 +63,4 @@ app.use("/cart", cartRouters);
 app.use("/payment", paymentRouters);
 app.use("/history", historyRouters);
 
-// app.listen(PORT, () => console.log("Api Running :", PORT));
-
-//Start Server
-server.listen(PORT, () => {
-  console.log("Socket server is running at port:", PORT);
-});
+app.listen(PORT, () => console.log("Api Running :", PORT));
